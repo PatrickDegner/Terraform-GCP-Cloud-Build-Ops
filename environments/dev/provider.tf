ec72256a84250f2d1f2ac11305b20da0c52f3d07
PatrickDegner <acaer@gmx.net>
2023-02-24 22:36:10 +0100

    creds

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -2,6 +2,6 @@ provider "google" {
   project = "terraform-project-378819"
   region = "europe-west3"
   zone = "europe-west3-a"
-  credentials = "../../key.json"
+  credentials = "gs://pd-terraform-backend/key.json"
 }
 
